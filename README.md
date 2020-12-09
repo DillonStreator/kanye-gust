a92055cc16cf8f99992e08e220d3caeea95dd6fb
adavila0703 <36370758+adavila0703@users.noreply.github.com>
2020-12-08 21:33:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# weatherly-kanye
+ Start your morning off with a random kanye quote, some coffee, and the weather
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+const fs = require("fs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+    const city = req.body.city;
+    const apiKey = "50e08ae00ca0e8874ffecb4c7a1e0104";
+    const weatherUrl ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+    const kanyeRest = "https://api.kanye.rest/";
+    https.get(kanyeRest, (response) => {
+        response.on("data", (data) => {
+            const kanyeQuote = JSON.parse(data).quote;
+            res.write("<h1>" + kanyeQuote + " - Kanye West</h1>");
+        });
+    });
+    https.get(weatherUrl, (response) => {
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const weatherTemp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const weatherIcon = weatherData.weather[0].icon;
+            const imageUrl =
+                "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+            res.write("<h1>The temperature in " + city + " is " + String(weatherTemp) + "F</h1>");
+            res.write('<img src="' + imageUrl + '">');
+            res.send();
+        });
+    });
+});
+
+app.listen(3000, () => {
+    console.log("success");
+});
