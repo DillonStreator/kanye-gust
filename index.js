a12db7017a497937d331d88a19853145c2f6d1e9
adavila0703 <36370758+adavila0703@users.noreply.github.com>
2020-12-09 15:50:22 -0600

    static files added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const https = require("https");
 const fs = require("fs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -15,23 +16,27 @@ app.post("/", (req, res) => {
     const apiKey = "50e08ae00ca0e8874ffecb4c7a1e0104";
     const weatherUrl ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
     const kanyeRest = "https://api.kanye.rest/";
+
     https.get(kanyeRest, (response) => {
-        response.on("data", (data) => {
+        response.once("data", (data) => {
             const kanyeQuote = JSON.parse(data).quote;
             res.write("<h1>" + kanyeQuote + " - Kanye West</h1>");
         });
     });
+
     https.get(weatherUrl, (response) => {
-        response.on("data", (data) => {
+        response.once("data", (data) => {
             const weatherData = JSON.parse(data);
             const weatherTemp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
             const weatherIcon = weatherData.weather[0].icon;
-            const imageUrl =
-                "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-            res.write("<h1>The temperature in " + city + " is " + String(weatherTemp) + "F</h1>");
+            const weatherFeelsLike = weatherData.main.feels_like;
+            const imageUrl ="http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+            res.write("<h1>The temperature in " + city + " is " + String(weatherTemp) + 
+            "F, feels like " + String(weatherFeelsLike) + "F, with " + String(weatherDescription) + "</h1>");
             res.write('<img src="' + imageUrl + '">');
             res.send();
+        
         });
     });
 });
